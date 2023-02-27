d4792b7c807f30b95fa28f118023d1e2f80b7d98
oraby <ma3oraby@gmail.com>
2023-02-27 14:50:32 +0200

    add brand single

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 
-from .views import ProductList , ProductDetail , add_review
+from .views import ProductList , ProductDetail , add_review , BrandList , BrandDetail
 
 app_name = 'product'
 
@@ -8,5 +8,7 @@ urlpatterns = [
     path('', ProductList.as_view(),name='product_list'),
     path('<slug:slug>',ProductDetail.as_view(),name='product_detail'),
     path('<slug:slug>/add_review',add_review,name='add_review'),
+    path('brands/' , BrandList.as_view() , name='brand_list'),
+    path('brands/<slug:slug>' , BrandDetail.as_view() , name='brand_detail'),
 
 ]
--- product/views.py
+++ product/views.py
@@ -1,37 +1,58 @@
 from django.shortcuts import render , redirect
-from .models import Product , Brand 
-from django.views.generic import ListView , DetailView 
-from .forms import ProductReviewForm 
+from .models import Product , Brand
+from django.views.generic import ListView , DetailView
+from .forms import ProductReviewForm
 
-
-# Create your views here.
-class ProductList (ListView):
+class ProductList(ListView):
     model = Product
     paginate_by = 50
     extra_context = {'all_count' : Product.objects.all().count()}
-
-class ProductDetail (DetailView): 
-    model =  Product 
+    
+    
+class ProductDetail(DetailView):
+    model = Product
+    
     
     def get_context_data(self, **kwargs):
         product = self.get_object()
         context = super().get_context_data(**kwargs)
-        context["related_products"] = Product.objects.filter(brand=product.brand)[:10]
-        return context 
-def add_review (request,slug):    
+        context["related_products"] = Product.objects.filter(brand=product.brand)[:10] 
+        return context
+    
+
+def add_review(request,slug):
     product = Product.objects.get(slug=slug)
-    if request.method == 'POST' : 
+    if request.method == 'POST':
         form = ProductReviewForm(request.POST)
         if form.is_valid():
             myform = form.save(commit=False)
-            myform.user = request.user 
-            myform.product = product 
-            myform.save() 
+            myform.user = request.user
+            myform.product = product
+            myform.save()
     return redirect(f'/products/{product.slug}')
 
 
-class BrandList (ListView) : 
+
+class BrandList(ListView):
     model = Brand
-
-
-
+    paginate_by = 50
+    extra_context = {'all_count' : Brand.objects.all().count()}
+    
+    
+    
+class BrandDetail(ListView):
+    model = Product
+    paginate_by = 50
+    template_name = 'product/brand_detail.html'
+    
+    def get_queryset(self):
+        brand = Brand.objects.get(slug=self.kwargs['slug'])
+        queryset = Product.objects.filter(brand=brand)
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["brand"] = Brand.objects.get(slug=self.kwargs['slug'])
+        return context
+    
+    
